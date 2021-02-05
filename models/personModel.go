8e60796936f26d4ee8f67c920877ab3f6e37982f
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-05 16:38:04 +0700

    feat(models): add base model

---
+++ models/personModel.go
@@ -0,0 +1,10 @@
+package models
+
+import "gorm.io/gorm"
+
+// Persons model
+type Persons struct {
+	gorm.Model
+	Nip, FullName, FirstName, LastName, BirthDate, BirthPlace, PhotoProfileUrl, Gender, ZoneLocation string
+	SubDistrictID                                                                                    uint
+}
