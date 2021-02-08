45426ddcd73c5f5046aebb6231d29b68b42fb371
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-08 10:53:29 +0700

    feat(controllers, route): add report person by gender

--- route/handleRoute.go
+++ route/handleRoute.go
@@ -36,7 +36,7 @@ func RouteHandler(app *gin.Engine) *gin.Engine {
 	app.POST("/auth/login", gorm.LoginUser)
 
 	// Upload
-	app.POST("/person/photo", authMiddleware, gorm.UploadSingle)
+	app.PATCH("/person/photo", authMiddleware, gorm.UploadSingle)
 
 	// Province CRUD Route
 	app.POST("/province", authMiddleware, gorm.PostCreateProvince)
